114e71950a1119ad1e848df2a1deab69265e047b
vishnu2006965 <vr9259@srmist.edu.in>
2026-02-17 10:43:22 +0530

    use cse 7

---
+++ Usecase7PalindromeCheckerApp.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class Usecase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        System.out.println(isPalindrome);
+
+        scanner.close();
+    }
+}
