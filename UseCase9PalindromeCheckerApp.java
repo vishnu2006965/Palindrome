a91d79411bde965389d3815b40c3d15d8fc586a3
vishnu2006965 <vr9259@srmist.edu.in>
2026-02-17 11:12:27 +0530

    use case 9

---
+++ UseCase9PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class UseCase9PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        boolean result = isPalindrome(input, 0, input.length() - 1);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+
+    public static boolean isPalindrome(String str, int start, int end) {
+
+        if (start >= end) {
+            return true;
+        }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
+}
