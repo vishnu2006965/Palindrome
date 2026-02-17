76a320b016b5d78869e0da7e10d9696f0433ba14
vishnu2006965 <vr9259@srmist.edu.in>
2026-02-17 10:38:47 +0530

    use acse 6

---
+++ UseCase6PalindromeCheckerApp.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class UseCase6PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        Queue<Character> queue = new LinkedList<>();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            queue.add(ch);
+            stack.push(ch);
+        }
+
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println(isPalindrome);
+
+        scanner.close();
+    }
+}
